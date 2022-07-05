722c44fa5162bad1a6ba8359bed323f28bd1433c
Jennifer <jenniferfgriffin@utexas.edu>
2022-07-05 11:00:16 -0500

    check win

--- app.js
+++ app.js
@@ -105,6 +105,8 @@ const sightWords = [
 let correctWord = []
 let wrongWord = []
 let underscore = []
+let answerArray = document.getElementsByClassName('answerArray')
+
 //get word button
 const getWordbtn = document.getElementById('btnword')
 
@@ -112,41 +114,41 @@ const getWordbtn = document.getElementById('btnword')
 let word = sightWords[Math.floor(Math.random() * sightWords.length)]
 console.log(word)
 // generating the mystery word
-let answerArray = document.getElementsByClassName('answerArray')
 
 let randomWord = () => {
   for (let i = 0; i < word.length; i++) {
     underscore.push('_')
   }
   return underscore
-  // let answerArray = []
-  // for (i = 0; i < word.length; i++) {
-  //   answerArray.append = '_'
-  // }
-  // let remainingLetters = word.length
-  // answer.innerhtml = answerArray[i]
 }
 answerArray[0].innerHTML = randomWord().join(' ')
-console.log(randomWord())
+
 // const addText = () => {
 //   document.body.append('It seems as if it has been clicked!')
 // }
 randomWord()
 getWordbtn.addEventListener('click', randomWord)
+//run game with keystrokes
 document.addEventListener('keypress', (event) => {
   let key = String.fromCharCode(event.keyCode)
   ///if guess right
+
   if (word.indexOf(key) > -1) {
     correctWord.push(key)
     console.log(correctWord)
     underscore[word.indexOf(key)] = key
-    underscore[0].innerHTML = underscore.join(' ')
-    console.log(underscore)
-    if (underscore.join('') === key) {
-      alert('Winner!')
+
+    if (underscore.join('') === word) {
+      return confirm('You win! Want to play again?')
     }
+
+    // answerArray[0].innerHTML = randomWord().join(' ')
+    // console.log(underscore)
+    //
+    // }
   } else {
     wrongWord.push(key)
     console.log(wrongWord)
   }
 })
+//
